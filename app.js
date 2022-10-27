6c18fd4d100bf7dd492d5c28fdb4f994e5399f37
jkresak101 <112973741+jkresak101@users.noreply.github.com>
2022-10-27 10:08:52 -0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,19 @@
+// Make the h1 in header have different text
+
+// Make <li> elements in the <ul> in the <nav> get larger when you hover over them
+
+// Add any element to the main element (Maybe a p? Dare you try to put an image in there?)
+
+// Dynamically change the color of the background on the footer when the user hovers over it
+
+// Change the font size of the paragraphs in the main element when the user clicks them
+
+// Change what the text says in the footer when the user clicks it
+
+// Remove/Toggle Visiblity the h1 element in the header (hint: do you already have this saved in a variable from earlier?)
+
+// Add the copyright back in to the footer in another element and change it to 2022 (or whatever year it might be. I'm just a voice in the past here. You are the future!)
+
+// Change the text in one of the paragraph tags in the main element
+
+// Change the width of the header element to 200px 
